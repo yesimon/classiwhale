b3247fee1e5d096416696f51f9e1a4c28547f1e7
Dan Huang <dan@Dan-Huangs-MacBook-Pro.local>
2010-07-20 19:46:19 -0700

    bitches

---
+++ dxm/tweed/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from dxm.tweet.models import Status
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    statuses = models.ManyToManyField(Status, blank=True, through='StatusDetails')
+    
+    def __unicode__(self):
+        return u'%s\'s Profile' % (self.user.username)
+    
+    class Meta:
+        ordering = ["user"]
+
+        
+class StatusDetails(models.Model):
+    user_profile = models.ForeignKey(UserProfile) # should be unique=True
+    status = models.ForeignKey(Status) # should be unique=True
+    rating = models.IntegerField(blank=True)
+    rated_time = models.DateTimeField(auto_now_add=True)
---
+++ dxm/tweed/accounts/signals.py
@@ -0,0 +1,3 @@
+from django.dispatch import Signal
+from django.contrib.auth.models import User
+from dxm.accounts.models import UserProfile
